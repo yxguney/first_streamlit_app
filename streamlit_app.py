1f4663589920362a199abcecad4c0cea4e0c0c2d
yxguney <113741044+yxguney@users.noreply.github.com>
2022-09-16 20:55:54 -0500

    Update streamlit_app.py
    add 3 lines text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Main Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spanich, Rocket Smoothy')
+streamlit.text('Hard boiled free range egg')
