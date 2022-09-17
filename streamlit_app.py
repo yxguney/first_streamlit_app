ef78b0525962a636f9081bccdd42127651583176
yxguney <113741044+yxguney@users.noreply.github.com>
2022-09-16 23:03:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,4 @@ fruit_listed = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-streamlit.dataframe(fruit_listed)
+
