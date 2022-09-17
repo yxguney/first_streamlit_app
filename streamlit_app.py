2b7cea57bc233ee4e508b01c14d8ed666dfad587
yxguney <113741044+yxguney@users.noreply.github.com>
2022-09-16 22:56:51 -0500

    Update streamlit_app.py
    add panda data

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.text('🥑🍞 Avacado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import panda
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
